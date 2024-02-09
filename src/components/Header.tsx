7ef5cf8bc500d20cdcd5eac9f62269b7eeabfd2c
Ryan O'Connor <rocsteady888@gmail.com>
2024-02-08 19:02:59 -0500

    intiial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Ryan O'Connor</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/screens/Skills.tsx
@@ -0,0 +1,9 @@
+function Skills() {
+  return (
+    <div>
+      <h1>Skill</h1>
+    </div>
+  );
+}
+
+export default Skills;
