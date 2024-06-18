0de905c588b050fd3e853cd9a760006103f1adfe
Ryan O'Connor <rocsteady888@gmail.com>
2024-06-18 09:00:35 +0000

    styling for smooth scrolling

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -4,72 +4,64 @@ export default function About() {
   return (
     <section id="about">
       <h2>Software Engineer and Data Scientist</h2>
-      <p>
-        As an experienced full stack software engineer and data scientist, I am deeply passionate about learning and
-        creating. My journey in software engineering began in 2013 while I was
-        studying to become a licensed Paramedic. The subpar software we used to
-        document our skills in the classroom and the field sparked a thought in
-        me, "I can build something better than this." This led me to start
-        learning to code, beginning with PHP and MySQL, and creating a basic web
-        application to track my progress in paramedic school.
-      </p>
-      <p>
-        After completing paramedic school, I attended a coding bootcamp at UNC
-        Chapel Hill. It was there that I was introduced to React JS, a
-        technology that has since become a cornerstone of my development work.
-        During the bootcamp, I met a like-minded engineer, and together we
-        cofounded a company aimed at replacing the inefficient software I had
-        experienced as a paramedic student. We successfully developed a fully
-        functional application that was adopted by my alma mater and another
-        school.
-      </p>
-      <p>
-        This entrepreneurial experience solidified my interest in software
-        development, prompting me to pursue a bachelor's degree in computer
-        science. Simultaneously, I worked on several projects, including an
-        e-commerce website and a cardiology learning platform.
-      </p>
-      <p>
-        I completed my bachelor's degree in 2019 and subsequently began working
-        with RENCI, an organization focused on providing solutions for
-        researchers. Here, I was often engaged in projects involving
-        asynchronous tools and services. My contributions included creating and
-        enhancing workflows, a role that allowed me to collaborate with numerous
-        talented engineers and researchers. At RENCI, my primary focus was on
-        frontend development using React and TypeScript, complemented by backend
-        tasks with Python/Django and Node.js.
-      </p>
-      <p>
-        Since 2021, I have been with flyExclusive, a private jet charter
-        company. In this role, I am involved in various projects, such as
-        developing a customer-facing website, a mobile app, and multiple
-        internal tools to improve business efficiency. My contributions have
-        been crucial in transitioning the company to a robust software
-        ecosystem, significantly aiding in its growth. My work primarily
-        revolves around mobile applications using React Native and Node.js in
-        TypeScript, and I have honed my skills in SQL while managing our
-        PostgreSQL database. An interesting aspect of my role involves creating
-        microservices in AWS to interface with Airtable, a tool used by our
-        internal stakeholders for data storage, thus providing a more robust API
-        for our frontend applications. Working at flyExclusive has provided me
-        with a unique experience of managing legacy code and innovating new
-        features from scratch.
-      </p>
-      <p>
-        In my personal time, I enjoy volunteering with Second Chance Animal
-        Rescue in Raleigh. My involvement includes assisting with adoption event
-        setups and fostering multiple dogs, helping them find loving homes.
-      </p>
-      <p>
-        My passion for software is similar to a scene from one of my favorite
-        Harry Potter movies, where Professor Slughorn describes a magical moment
-        created by Harry's mother. The transformation of a flower petal into a
-        fish in a bowl of water, described as "beautiful magic, wondrous to
-        behold," resonates with my love for creating software. When I see a
-        stakeholder using software I have developed to solve their problem it
-        sparks joy for me. I am excited to continue my journey in software
-        engineering and am eager to see what the future holds.
-      </p>
+      <div className="content-container">
+        <p>
+          As an experienced full stack software engineer and data scientist, I am deeply passionate about learning and
+          creating. My journey in software engineering began in 2013 while I was
+          studying to become a licensed Paramedic. The subpar software we used to
+          document our skills in the classroom and the field sparked a thought in
+          me, "I can build something better than this." This led me to start
+          learning to code, beginning with PHP and MySQL, and creating a basic web
+          application to track my progress in paramedic school.
+        </p>
+        <p>
+          After completing paramedic school, I attended a coding bootcamp at UNC
+          Chapel Hill. It was there that I was introduced to React JS, a
+          technology that has since become a cornerstone of my development work.
+          During the bootcamp, I met a like-minded engineer, and together we
+          cofounded a company aimed at replacing the inefficient software I had
+          experienced as a paramedic student. We successfully developed a fully
+          functional application that was adopted by my alma mater and another
+          school.
+        </p>
+        <p>
+          This entrepreneurial experience solidified my interest in software
+          development, prompting me to pursue a bachelor's degree in computer
+          science. Simultaneously, I worked on several projects, including an
+          e-commerce website and a cardiology learning platform.
+        </p>
+        <p>
+          I completed my bachelor's degree in 2019 and subsequently began working
+          with RENCI, an organization focused on providing solutions for
+          researchers. Here, I was often engaged in projects involving
+          asynchronous tools and services. My contributions included creating and
+          enhancing workflows, a role that allowed me to collaborate with numerous
+          talented engineers and researchers. At RENCI, my primary focus was on
+          frontend development using React and TypeScript, complemented by backend
+          tasks with Python/Django and Node.js.
+        </p>
+        <p>
+          Since 2021, I have been with flyExclusive, a private jet charter
+          company. In this role, I am involved in various projects, such as
+          developing a customer-facing website, a mobile app, and multiple
+          internal tools to improve business efficiency. My contributions have
+          been crucial in transitioning the company to a robust software
+          ecosystem, significantly aiding in its growth. My work primarily
+          revolves around mobile applications using React Native and Node.js in
+          TypeScript, and I have honed my skills in SQL while managing our
+          PostgreSQL database. An interesting aspect of my role involves creating
+          microservices in AWS to interface with Airtable, a tool used by our
+          internal stakeholders for data storage, thus providing a more robust API
+          for our frontend applications. Working at flyExclusive has provided me
+          with a unique experience of managing legacy code and innovating new
+          features from scratch.
+        </p>
+        <p>
+          In my personal time, I enjoy volunteering with Second Chance Animal
+          Rescue in Raleigh. My involvement includes assisting with adoption event
+          setups and fostering multiple dogs, helping them find loving homes.
+        </p>
+      </div>
     </section>
   );
 }
--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,9 +1,14 @@
+import '../index.css';
+
 function Main() {
   return (
     <section id="main">
-      <h1>Ryan O'Connor</h1>
+        <h1>Ryan O'Connor</h1>
+        <p>Software Engineer & Data Scientist</p>
+        <p>Creating impactful solutions through code and data.</p>
     </section>
   );
 }
 
 export default Main;
+
