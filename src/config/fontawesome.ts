5086ca808e730d12817ebd45f630627cb79f0a0a
ryanoconnor285 <rocsteady888@gmail.com>
2024-11-14 07:58:00 -0500

    changes

--- src/config/fontawesome.ts
+++
@@ -1,8 +0,0 @@
-// src/config/fontawesome.ts
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fab } from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-
-export function initFontAwesome() {
-  library.add(fab, faCheckSquare, faCoffee);
-}
